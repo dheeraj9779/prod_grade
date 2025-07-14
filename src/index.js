2ee7328ff11fb43b1cd68900986390b4519d97a4
Parasdheeraj <angulardevloper@parastechnologies.com>
2025-07-14 16:05:28 +0530

    Connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config({
+    path: './.env'
+})
+
+import connectDB from './db/database_conn.js';
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+//         app.on("error", (err) => {
+//             console.error('Error in Express app:', err);
+//             throw err;
+//         });
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         });
+//     }
+//     catch (error) {
+//         console.error('Error connecting to MongoDB:', error);
+//         throw error;
+//     }
+// })()
